97d238f08dcc838cf0f1958412c0f258989dd612
wizard32 <wizard32@wp.pl>
2017-11-22 12:58:20 +0100

    Add queries plaground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,29 @@
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose.js');
+const {Todo} = require('./../server/models/todo.js');
+
+var id = '5a154fbebb219b0013933781aa';
+
+if (!ObjectID.isValid(id)) {
+	console.log('Id not valid');
+}
+/*Todo.find({
+	_id: id
+}).then((todos) => {
+	console.log('todos', todos);
+});
+
+Todo.findOne({
+	_id: id
+}).then((todo) => {
+	console.log('todo', todo);
+});*/
+
+Todo.findById(id).then((todo) => {
+	if (!todo) {
+		return console.log('Id not found');
+	}
+	console.log('todo by id', todo);
+}).catch((e) => {
+	console.log('Error', e);
+});
