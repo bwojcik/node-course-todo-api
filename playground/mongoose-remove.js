6eac3c376dc0dcf3107e92d5943b8fac3433f7cc
wizard32 <wizard32@wp.pl>
2017-11-22 18:35:02 +0100

    Add DELETE /todos/:id route

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,29 @@
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose.js');
+const {Todo} = require('./../server/models/todo.js');
+
+var id = '5a154fbebb219b0013933781';
+
+if (!ObjectID.isValid(id)) {
+	console.log('Id not valid');
+}
+/*Todo.find({
+	_id: id
+}).then((todos) => {
+	console.log('todos', todos);
+});*/
+
+Todo.findOneAndRemove({
+	_id: id
+}).then((todo) => {
+	console.log('todo', todo);
+});
+
+/*Todo.findByIdAndRemove(id).then((todo) => {
+	if (!todo) {
+		return console.log('Id not found');
+	}
+	console.log('todo by id', todo);
+}).catch((e) => {
+	console.log('Error', e);
+});*/
--- server/server.js
+++ server/server.js
@@ -50,6 +50,23 @@ app.get('/todos/:id', (request, response) => {
 	});
 });
 
+app.delete('/todos/:id', (request, response) => {
+	var id = request.params.id;
+
+	if (!ObjectId.isValid(id)) {
+		response.status(404).send();
+	}
+
+	Todo.findByIdAndRemove(id).then((todo) => {
+		if (!todo) {
+			response.status(404).send();
+		}
+		response.send({todo});
+	}).catch((e) => {
+		response.status(400).send();
+	});
+});
+
 app.listen(port, () => {
 	console.log(`Started on port ${port}`);
 });
